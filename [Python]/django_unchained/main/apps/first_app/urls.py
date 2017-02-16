429478cb6be38c7858a9c908cfcfd9feeb511c2f
jaebooker <jaesonbooker@gmail.com>
2017-02-15 19:17:02 -0800

    random

--- [Python]/django_unchained/main/apps/first_app/urls.py
+++ [Python]/django_unchained/main/apps/first_app/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from . import views           # This line is new!
+from . import views
 urlpatterns = [
-    url(r'^$', views.yourMethodFromUrls),     # This line has changed!
+    url(r'^$', views.yourMethodFromUrls),
+    url(r'^create$', views.create)
 ]
--- [Python]/django_unchained/main/apps/first_app/views.py
+++ [Python]/django_unchained/main/apps/first_app/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, HttpResponse
-from datetime import datetime
+from django.shortcuts import render, HttpResponse, redirect
+import random
+import string
 
 def yourMethodFromUrls(request):
-    context = {
-    "somekey": str(datetime.now())
-    }
-    return render(request,'first_app/index.html', context)
+    if 'num' not in request.session:
+        request.session['num'] = 0
+    if 'count' not in request.session:
+        request.session['count'] = 1
+    return render(request,'first_app/index.html')
+def create(request):
+    stringletters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    newstring = ""
+    if request.method == 'POST':
+        for x in range(0, 20):
+            newstring += random.choice(stringletters)
+        request.session['num'] = newstring
+        request.session['count'] += 1
+        return redirect('/')
+    else:
+        return redirect('/')
