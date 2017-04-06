9e602933c0a23126ebc75f784c60483fdc25c7bc
jaebooker <jaesonbooker@gmail.com>
2017-04-05 18:06:31 -0700

    mean stack

---
+++ [MEAN]/module_express/app.js
@@ -0,0 +1,5 @@
+var my_module = require('./modulex');
+my_module.addto(400,20);
+my_module.multiply(5,5);
+my_module.square(5);
+my_module.random();
---
+++ [MEAN]/module_express/modulex.js
@@ -0,0 +1,14 @@
+module.exports = {
+    addto: function(num1, num2) {
+         console.log(num1 + num2)
+    },
+    multiply: function(num1, num2) {
+         console.log(num1 * num2)
+    },
+    square: function(num) {
+         console.log(num * num)
+    },
+    random: function() {
+         console.log(Math.floor(Math.random(100)*100))
+    }
+};
