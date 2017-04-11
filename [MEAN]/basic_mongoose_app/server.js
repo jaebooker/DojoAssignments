f4854bade89ac3d8484940d80d56ececd1e766cf
jaebooker <jaesonbooker@gmail.com>
2017-04-11 12:24:23 -0700

    mongoose

--- [MEAN]/basic_mongoose_app/server.js
+++ [MEAN]/basic_mongoose_app/server.js
@@ -15,58 +15,47 @@ app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
 // Setting our View Engine set to EJS
 app.set('view engine', 'ejs');
-// Routes
-// Root Request
 app.get('/', function(req, res) {
     // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
     res.render('index');
 })
-// Add User Request
-app.post('/users', function(req, res) {
-    console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    res.redirect('/');
-})
-// Setting our Server to Listen on Port: 8000
-app.listen(8000, function() {
-    console.log("listening on port 8000");
-})
+// Routes
+// Root Request
 // This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
 //   our db in mongodb -- this should match the name of the db you are going to use for your project.
 mongoose.connect('mongodb://localhost/basic_mongoose');
 var UserSchema = new mongoose.Schema({
  name: String,
- age: Number
+ quote: String
 })
 mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
 var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
 // This is the route that we already have in our server.js
 // When the user presses the submit button on index.ejs it should send a post request to '/users'.  In
 //  this route we should add the user to the database and then redirect to the root route (index view).
-mongoose.Promise = spherical.Promise;
-app.post('/users', function(req, res) {
-  console.log("POST DATA", req.body);
+//mongoose.Promise = spherical.Promise;
+// The root route -- we want to get all of the users from the database and then render the index view passing it all of the users
+app.post('/post', function(req, res) {
+  console.log("POST", req.body);
+  console.log("anything")
   // create a new User with the name and age corresponding to those from req.body
-  var user = new User({name: req.body.name, age: req.body.age});
+  var user = new User({name: req.body.name, quote: req.body.quote});
   // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-  user.save(function(err) {
+  user.save(function(err, user) {
     // if there is an error console.log that something went wrong!
     if(err) {
       console.log('something went wrong');
     } else { // else console.log that we did well and then redirect to the root route
-      console.log('suddenly added a user!');
-      res.redirect('/');
+      console.log('suddenly added a quote!');
+      res.redirect('quotes');
     }
   })
 })
-// The root route -- we want to get all of the users from the database and then render the index view passing it all of the users
-app.get('/', function(req, res) {
-  User.find({}, function(err, users) {
-    // This is the method that finds all of the users from the database
-    // Notice how the first parameter is the options for what to find and the second is the
-    //   callback function that has an error (if any) and all of the users
-    // Keep in mind that everything you want to do AFTER you get the users from the database must
-    //   happen inside of this callback for it to be synchronous
-    // Make sure you handle the case when there is an error, as well as the case when there is no error
+app.get('/quotes', function(req, res) {
+  User.find({}, function(err, user) {
+    res.render('quotes', {user: user});
   })
+})
+app.listen(8000, function() {
+    console.log("listening on port 8000");
 })
